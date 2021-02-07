5c890bbce0b8d6cad3fb07e6b9c059d12a451b7e
Thomas Metcalfe <tommetcalfe@live.com>
2021-02-07 23:16:55 +0000

    Use torch in vec env

--- stable_baselines3/common/vec_env/dummy_vec_env.py
+++ stable_baselines3/common/vec_env/dummy_vec_env.py
@@ -4,6 +4,7 @@ from typing import Any, Callable, List, Optional, Sequence, Type, Union
 
 import gym
 import numpy as np
+import torch
 
 from stable_baselines3.common.vec_env.base_vec_env import VecEnv, VecEnvIndices, VecEnvObs, VecEnvStepReturn
 from stable_baselines3.common.vec_env.util import copy_obs_dict, dict_to_obs, obs_space_info
@@ -28,9 +29,19 @@ class DummyVecEnv(VecEnv):
         obs_space = env.observation_space
         self.keys, shapes, dtypes = obs_space_info(obs_space)
 
-        self.buf_obs = OrderedDict([(k, np.zeros((self.num_envs,) + tuple(shapes[k]), dtype=dtypes[k])) for k in self.keys])
-        self.buf_dones = np.zeros((self.num_envs,), dtype=bool)
-        self.buf_rews = np.zeros((self.num_envs,), dtype=np.float32)
+        self.buf_obs = OrderedDict(
+            [
+                (
+                    k,
+                    torch.from_numpy(
+                        np.zeros((self.num_envs,) + tuple(shapes[k]), dtype=dtypes[k])
+                    ).cuda(),
+                )
+                for k in self.keys
+            ]
+        )
+        self.buf_dones = torch.zeros((self.num_envs,), dtype=torch.bool).cuda()
+        self.buf_rews = torch.zeros((self.num_envs,), dtype=torch.float32).cuda()
         self.buf_infos = [{} for _ in range(self.num_envs)]
         self.actions = None
         self.metadata = env.metadata
@@ -48,7 +59,7 @@ class DummyVecEnv(VecEnv):
                 self.buf_infos[env_idx]["terminal_observation"] = obs
                 obs = self.envs[env_idx].reset()
             self._save_obs(env_idx, obs)
-        return (self._obs_from_buf(), np.copy(self.buf_rews), np.copy(self.buf_dones), deepcopy(self.buf_infos))
+        return (self._obs_from_buf(), deepcopy(self.buf_rews), deepcopy(self.buf_dones), deepcopy(self.buf_infos))
 
     def seed(self, seed: Optional[int] = None) -> List[Union[None, int]]:
         seeds = list()
